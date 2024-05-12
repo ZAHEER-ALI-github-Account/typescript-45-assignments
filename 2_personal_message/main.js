9abd5560f5a7bcdb1f240592c3832bd6d4605435
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-12 18:01:13 +0500

    45 assignments

---
+++ 2_personal_message/main.js
@@ -0,0 +1,4 @@
+//question no 2
+var person = "zaheer ali";
+var message = "would you like to learn python today.";
+console.log("hello ".concat(person, ",").concat(message));
---
+++ 2_personal_message/main.ts
@@ -0,0 +1,4 @@
+//question no 2
+let person:string="zaheer ali";
+let message:string="would you like to learn python today.";
+console.log(`hello ${person},${message}`);
