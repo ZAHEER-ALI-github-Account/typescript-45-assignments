96fb8096632c5bc9989f3f69221297981487a3f9
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-13 12:18:25 +0500

    45_assignments

---
+++ 13_your_own_array/main.ts
@@ -0,0 +1,5 @@
+//question no 13
+let transportation:string[]=["motorcycle","car","cycle","airoplane"]
+for(let i=0;i<transportation.length;i++){
+    console.log(`I would like to own a ` + transportation[i]);
+}
