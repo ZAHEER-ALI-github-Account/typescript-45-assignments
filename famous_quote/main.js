6f12d72a7306150f6c6d8f5f6069dc6ac3c2fa1b
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-13 11:58:36 +0500

    Delete famous_quote directory

--- famous_quote/main.js
+++
@@ -1,3 +0,0 @@
-//question no 4
-var famous_person = "albert einstein";
-console.log("".concat(famous_person, " once said,\"a person who never make a mistake never tried anything new\""));
--- famous_quote/main.ts
+++
@@ -1,3 +0,0 @@
-//question no 4
-let famous_person:string="albert einstein";
-console.log(`${famous_person} once said,"a person who never make a mistake never tried anything new."`);
