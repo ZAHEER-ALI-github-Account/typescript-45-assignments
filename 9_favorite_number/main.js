c1361e779c00b21c2d2155a00b36bbaf241028b1
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-13 12:13:31 +0500

    45_assignments

---
+++ 9_favorite_number/main.js
@@ -0,0 +1,4 @@
+//question no 9
+var favoritenumber = 9;
+var message = "my favorite number is ".concat(favoritenumber);
+console.log(message);
---
+++ 9_favorite_number/main.ts
@@ -0,0 +1,4 @@
+//question no 9
+let favoritenumber:number= 9;
+let message:string= `my favorite number is ${favoritenumber}`;
+console.log(message);
