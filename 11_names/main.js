385f8a9722f6d0d379a84ec8d20ebebedc35ef3a
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-13 12:16:14 +0500

    45_assignments

---
+++ 11_names/main.js
@@ -0,0 +1,5 @@
+var friends = ["Zaheer", "Ali", "Zakheer", "Adil", "Asadullah"];
+var message = "here is your good name:";
+for (var i = 0; i < friends.length; i++) {
+    console.log(message + friends[i]);
+}
---
+++ 11_names/main.ts
@@ -0,0 +1,6 @@
+//question no 11
+let friends:string[]=["Zaheer","Ali","Zakheer","Adil","Asadullah"];
+let message:string="here is your good name:";
+for (let i = 0; i < friends.length; i++){
+    console.log(message + friends[i]);
+}
