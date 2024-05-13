99847ed6f419e52e550c29d08b3326ed3f3420f3
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-13 12:19:20 +0500

    45_assignments

---
+++ 14_guest_list/main.js
@@ -0,0 +1,5 @@
+//question no 14
+var guest_list = ["zakheer", "asad", "sami"];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("Dear mr." + guest_list[i] + ",\n\nIt is our pleasure to invite you in our party" + ",\n\nThank you!\n\n");
+}
---
+++ 14_guest_list/main.ts
@@ -0,0 +1,5 @@
+//question no 14
+let guest_list:string[]=["zakheer","asad","sami"];
+for(let i=0;i<guest_list.length;i++){
+    console.log(`Dear mr.` + guest_list[i]+`,\n\nIt is our pleasure to invite you in our dinner`+`,\n\nThank you!\n\n`);
+}
