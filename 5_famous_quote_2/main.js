ba4ab4de8033ce616be55e3e4684a7fffb13ec54
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-13 12:02:31 +0500

    45 assignments

---
+++ 5_famous_quote_2/main.js
@@ -0,0 +1,4 @@
+//question no 5
+var famous_person = "Hazrat Ali R.A";
+var quote = "do not slave to others when allah has created you free";
+console.log("".concat(famous_person, " said,").concat(quote));
---
+++ 5_famous_quote_2/main.ts
@@ -0,0 +1,4 @@
+//question no 5
+let famous_person:string="Hazrat Ali R.A";
+let quote:string="do not slave to others when allah has created you free.";
+console .log(`${famous_person} said,${quote}`);
