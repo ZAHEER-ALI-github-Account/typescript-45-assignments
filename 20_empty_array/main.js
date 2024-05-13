40cc659ff8fa0de626c8e7921e978e38b4328d90
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-13 12:26:53 +0500

    45_assignments

---
+++ 20_empty_array/main.js
@@ -0,0 +1,11 @@
+//question no 19
+//Method 1
+var items1 = ["Pakistan", "Sindh", "Karachi", "Urdu"];
+console.log(items1);
+//method 2
+var items2 = [];
+items2.push("Germany");
+items2.push("America");
+items2.push("Russia");
+items2.push("Turkey");
+console.log(items2);
---
+++ 20_empty_array/main.ts
@@ -0,0 +1,13 @@
+//question no 20
+
+//Method 1
+let items1: string[] = ["Pakistan","Sindh","Karachi","Urdu"];
+console.log(items1);
+
+//method 2
+let items2: string[] = [];
+items2.push(`Germany`);
+items2.push(`America`);
+items2.push(`Russia`);
+items2.push(`Turkey`);
+console.log(items2);
