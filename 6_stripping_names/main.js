d3a04a7a1ea586ff42a0fe96d7e97985e2b0be97
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-13 12:05:56 +0500

    45_assignments

---
+++ 6_stripping_names/main.js
@@ -0,0 +1,4 @@
+//question no 6
+var personName = "\n\t Zaheer Ali \n\t";
+console.log(personName);
+console.log(personName.trim());
---
+++ 6_stripping_names/main.ts
@@ -0,0 +1,4 @@
+//question no 6
+let personName:string="\n\t Zaheer Ali \n\t";
+console.log(personName);
+console.log(personName.trim());
