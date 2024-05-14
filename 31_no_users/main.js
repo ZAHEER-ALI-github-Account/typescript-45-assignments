92aa2655d6b905b707bf18918ffaaebc311e1134
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 15:50:55 +0500

    45_assignments

---
+++ 31_no_users/main.js
@@ -0,0 +1,9 @@
+//question no 31
+var userNames = ["zaheer", "zakheer", "asad", "subhan", "ali"];
+if (userNames.length === 0) {
+    console.log("we need to find some users!");
+}
+else {
+    userNames = [];
+    console.log("All users names have been removed. " + userNames.length);
+}
---
+++ 31_no_users/main.ts
@@ -0,0 +1,8 @@
+//question no 31
+let userNames : string[] =["zaheer","zakheer","asad","subhan","ali"];
+if (userNames.length === 0){
+    console.log(`we need to find some users!`);
+} else {
+    userNames = [];
+    console.log(`All users names have been removed. ` + userNames.length);
+}
