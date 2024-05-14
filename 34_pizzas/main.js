5c1e6f1e278df1279b1886725803dce32fb73fe8
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 16:01:40 +0500

    45_assignments

---
+++ 34_pizzas/main.js
@@ -0,0 +1,7 @@
+//questio no 34
+var pizzas = ["afghani", "fajita", "tikka"];
+for (var _i = 0, pizzas_1 = pizzas; _i < pizzas_1.length; _i++) {
+    var pizza = pizzas_1[_i];
+    console.log("I like ".concat(pizza, " pizza."));
+}
+console.log("I really like pizza!");
---
+++ 34_pizzas/main.ts
@@ -0,0 +1,6 @@
+//questio no 34
+let pizzas: string[] =["afghani","fajita","tikka"];
+for (let pizza of pizzas){
+    console.log(`I like ${pizza} pizza.`);
+}
+console.log(`I really like pizza!`);
