b2f3eb055128de781d68416127d645be90b4714a
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 16:07:04 +0500

    45_assignments

---
+++ 36_T-shirts/main.js
@@ -0,0 +1,7 @@
+//question no 36
+function make_shirt(size, text) {
+    console.log("creating a ".concat(size, " shirt with a message: ").concat(text));
+}
+make_shirt("small", "Pakistan zindabad");
+make_shirt("medium", "Pakistan zindabad");
+make_shirt("large", "Pakistan zindabad");
---
+++ 36_T-shirts/main.ts
@@ -0,0 +1,7 @@
+//question no 36
+function make_shirt(size: string , text: string){
+    console.log(`creating a ${size} shirt with a message: ${text}`);
+}
+make_shirt(`small`,`Pakistan zindabad`);
+make_shirt(`medium`,`Pakistan zindabad`);
+make_shirt(`large`,`Pakistan zindabad`);
