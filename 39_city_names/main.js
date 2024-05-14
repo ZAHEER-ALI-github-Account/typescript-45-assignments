711fc3e4da552797715c5241f01724d3cd6de4b7
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 16:16:12 +0500

    45_assignments

---
+++ 39_city_names/main.js
@@ -0,0 +1,7 @@
+//question no 39
+function city_country(city, country) {
+    return "".concat(city, ",").concat(country);
+}
+console.log(city_country("karachi", "Pakistan"));
+console.log(city_country("jiddah", "saudi arabia"));
+console.log(city_country("los angeles", "america"));
---
+++ 39_city_names/main.ts
@@ -0,0 +1,7 @@
+//question no 39
+function city_country(city: string, country: string){
+    return `${city},${country}`;
+}
+console.log(city_country(`karachi`,`Pakistan`));
+console.log(city_country(`jiddah`,`saudi arabia`));
+console.log(city_country(`los angeles`,`america`));
