e3122e1a503063fed272365dd5b0d9fdb64c55b4
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 15:47:45 +0500

    45_assignments

---
+++ 29_favorite_fruit/main.ts
@@ -0,0 +1,17 @@
+//question no 29
+let favorite_fruits : string[] = ["mango","strawberries","banana"];
+if (favorite_fruits.includes("mango")){
+    console.log(`i really like mango!`);
+}
+if (favorite_fruits.includes("strawberries")){
+    console.log(`i really like strawberries!`);
+}
+if (favorite_fruits.includes("banana")){
+    console.log(`i really like banana!`);
+}
+if (favorite_fruits.includes("orange")){
+    console.log(`i really like orange!`);
+}
+if (favorite_fruits.includes("grapes")){
+    console.log(`i really like grapes!`);
+}
