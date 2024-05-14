7cd8919275da14d9251abfba60b42ed56516c2f0
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 16:22:27 +0500

    45_assignments

---
+++ 41_magicians/main.js
@@ -0,0 +1,8 @@
+//question no 41
+var magician = ["harry potter", "hermione granger", "ron weasley", "albus dumbledore"];
+function show_magicians(magicians) {
+    magicians.forEach(function (element) {
+        console.log(element);
+    });
+}
+show_magicians(magician);
---
+++ 41_magicians/main.ts
@@ -0,0 +1,10 @@
+//question no 41
+let magician : string[] = ["harry potter","hermione granger","ron weasley","albus dumbledore"];
+
+function show_magicians (magicians: string[]){
+
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magician);
