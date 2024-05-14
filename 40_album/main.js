8ce85195f9b7ce7ac7c5b36d71bdb2ff9ce20197
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 16:20:09 +0500

    45_assignments

---
+++ 40_album/main.js
@@ -0,0 +1,17 @@
+//question no 40
+function make_album(artist, title, tracks) {
+    var album = {
+        artist: artist,
+        title: title
+    };
+    if (tracks !== undefined) {
+        album.tracks = tracks;
+    }
+    return album;
+}
+var album1 = make_album("artist 1", "album title 1");
+console.log(album1);
+var album2 = make_album("artist 2", "album title 2");
+console.log(album2);
+var album3 = make_album("artist 3", "album title 3", 12);
+console.log(album3);
