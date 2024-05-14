1396f67230ffc8ca6fd5db9bcd2c5d5877d40b77
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 15:29:10 +0500

    45_assignments

---
+++ 22_international_error/main.js
@@ -0,0 +1,6 @@
+//question no 22
+var array = ["rado", "rolex", "casio", "versace"];
+//index error:
+console.log(array[5]);
+//correcting error:
+console.log(array);
---
+++ 22_international_error/main.ts
@@ -0,0 +1,8 @@
+//question no 22
+let array : string []=["rado","rolex","casio","versace"];
+
+//index error:
+console.log(array[5]);
+
+//correcting error:
+console.log(array);
