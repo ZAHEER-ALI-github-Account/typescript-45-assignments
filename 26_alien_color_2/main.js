654772ca9c32ade668669678163c59a04e334c36
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 15:35:59 +0500

    45_assignments

---
+++ 26_alien_color_2/main.js
@@ -0,0 +1,17 @@
+//question no 26
+//if the alien color is green:
+var alien_color = "green";
+if (alien_color === "green") {
+    console.log("Player just earn 5 points for shooting the alien.");
+}
+else {
+    console.log("Player just earn 10 points.");
+}
+//if the alien color is not green:
+alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("Player just earn 5 points for shooting the alien.");
+}
+else {
+    console.log("Player just earn 10 points.");
+}
---
+++ 26_alien_color_2/main.ts
@@ -0,0 +1,18 @@
+//question no 26
+//if the alien color is green:
+let alien_color : string = "green";
+if(alien_color === "green"){
+    console.log("Player just earn 5 points for shooting the alien.");
+}
+else{
+    console.log("Player just earn 10 points.");
+}
+
+//if the alien color is not green:
+alien_color = "yellow";
+if(alien_color === "green"){
+    console.log("Player just earn 5 points for shooting the alien.");
+}
+else{
+    console.log("Player just earn 10 points.");
+}
