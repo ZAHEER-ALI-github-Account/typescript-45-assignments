43a36401e17186eb9e2d74f12d23f7e6c60e57c9
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 15:30:52 +0500

    45_assignments

---
+++ 23_conditional_tests/main.ts
@@ -0,0 +1,43 @@
+//question no 23
+
+let car : string = "dodge";
+
+//test 1:
+console.log("Is car == `dodge`? I predict true.");
+console.log(car === `dodge`);
+
+//test 2:
+console.log("Is car == `dodge`? I predict true.");
+console.log(car == `dodge`);
+
+//test 3:
+console.log("Is car != `honda`? I predict true.");
+console.log(car != `honda`);
+
+//test 4:
+console.log("Is car !== `suzuki`? I predict true.");
+console.log(car !== `suzuki`);
+
+//test 5:
+console.log("Is car.upper case == `DODGE`? I predict true.");
+console.log(car.toUpperCase() == `DODGE`);
+
+//test 6:
+console.log("Is car == `DODGE`? I predict false.");
+console.log(car == `DODGE`);
+
+//test 7:
+console.log("Is car === `DODGE`? I predict false.");
+console.log(car === `DODGE`);
+
+//test 8:
+console.log("Is car === `train`? I predict false.");
+console.log(car === `train`);
+
+//test 9:
+console.log("Is car === `van`? I predict false.");
+console.log(car === `van`);
+
+//test 10:
+console.log("Is car === `airoplane`? I predict false.");
+console.log(car === `airoplane`);
