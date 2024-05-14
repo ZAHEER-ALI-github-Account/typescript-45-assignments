10dfa724bbb92761bbe7b2d1a6304e6efa536115
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 16:04:02 +0500

    45_assignments

---
+++ 35_animals/main.ts
@@ -0,0 +1,6 @@
+//question no 35
+let animals: string[] = ["horse","dog","eagle"];
+for (let animal of animals){
+    console.log(`A ${animal} would make a great pet.`);
+}
+console.log(`Any of these animals would make a great pet!`);
