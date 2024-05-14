f45931bd971bc31e6e81e58b0386583125a6f4f8
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2024-05-14 15:37:58 +0500

    45_assignments

---
+++ 27_alien_color_3/main.js
@@ -0,0 +1,43 @@
+//question no 27
+//version 1:
+var alien_color = "green";
+if (alien_color === "green") {
+    console.log("the player earn 5 points.");
+}
+else if (alien_color === "yellow") {
+    console.log("the player earn 10 points.");
+}
+else if (alien_color === "red") {
+    console.log("the player earn 15 points.");
+}
+else {
+    console.log("please select right color.");
+}
+//version 2:
+alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("the player earn 5 points.");
+}
+else if (alien_color === "yellow") {
+    console.log("the player earn 10 points.");
+}
+else if (alien_color === "red") {
+    console.log("the player earn 15 points.");
+}
+else {
+    console.log("please select right color.");
+}
+//version 3:
+alien_color = "red";
+if (alien_color === "green") {
+    console.log("the player earn 5 points.");
+}
+else if (alien_color === "yellow") {
+    console.log("the player earn 10 points.");
+}
+else if (alien_color === "red") {
+    console.log("the player earn 15 points.");
+}
+else {
+    console.log("please select right color.");
+}
